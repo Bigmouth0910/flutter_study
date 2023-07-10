a2f8969668d586372b9b96cf83e8a90b7945bdaa
bigm <you@example.com>
2023-07-10 13:00:22 +0900

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -57,4 +57,25 @@ class MyApp extends StatelessWidget {
           )),
     );
   }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
